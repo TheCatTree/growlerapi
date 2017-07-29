3016078b5d377fa7ac361ad700d4d87d852e233a
nealhartley <neal.w.hartley@gmail.com>
2017-07-29 19:19:39 +1200

    added routes for parks

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ var User = require('./schemas/user');
 
 //link to models in db of each object
 Dog_Model =  db.model('Dog');
+Park_Model = db.model('Park');
+Review_Model = db.model('Review');
+User_Model = db.model('User');
 
 
 var app = express();
@@ -43,6 +46,9 @@ router.use(function(req, res, next) {
     next(); // make sure we go to the next routes and don't stop here
 });
 
+
+/////Routes for dogssss/////////////////////////////////
+////////////////////////////////////////////////////////
 router.route('/dogs')
     //create a dog
     .post( function(req, res){
@@ -85,8 +91,6 @@ router.route('/dogs/:dog_id')
         var id = new ObjectId(req.params.dog_id.toString());
 
 
-        console.log(id);
-
       Dog_Model.findById( id , function(err, dog){
         if(err){res.send(err);}
 
@@ -94,6 +98,230 @@ router.route('/dogs/:dog_id')
 
       });
 
+
+    })
+    //update the doggo with new info
+    .put(function(req, res){
+        // use our dog model to find the dog we want
+        //lets convert into an object id
+        var ObjectId = mongoose.Types.ObjectId;
+        var id = new ObjectId(req.params.dog_id.toString());
+
+
+        Dog_Model.findById( id , function(err, dog){
+            if(err){res.send(err);}
+
+            if(req.body.name!==undefined){
+                dog.name = req.body.name;
+                console.log("updated name")
+            }
+
+            else if(req.body.picture!==undefined){
+                dog.picture = req.body.picture;
+                console.log("updated picture")
+            }
+
+            else if(req.body.description!==undefined){
+                dog.description = req.body.description;
+                console.log("updated description")
+            }
+
+            else if(req.body.breed!==undefined){
+                dog.breed = req.body.breed;
+                console.log("updated breed")
+            }
+
+            // save the dog
+            dog.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json(dog);
+            });
+
+
+        });
+
+    });
+
+router.route('/dogs/:dog_id/befriend/:friend_id')
+    //adding a friend to a dogs friend list
+    .put(function(req, res){
+        // use our dog model to find the dog we want
+        //lets convert into an object id
+        var ObjectId = mongoose.Types.ObjectId;
+        var id = new ObjectId(req.params.dog_id.toString());
+
+
+        Dog_Model.findById( id , function(err, dog){
+
+            dog.friends.push(req.params.friend_id.toString());
+
+            // save the dog
+            dog.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json(dog);
+            });
+        })
+
+    });
+
+router.route('/dogs/:dog_id/addattr/:_attr')
+//adding a friend to a dogs friend list
+    .put(function(req, res){
+        // use our dog model to find the dog we want
+        //lets convert into an object id
+        var ObjectId = mongoose.Types.ObjectId;
+        var id = new ObjectId(req.params.dog_id.toString());
+
+
+        Dog_Model.findById( id , function(err, dog){
+
+            dog.friends.push(req.params._attr.toString());
+
+            // save the dog
+            dog.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json(dog);
+            });
+        })
+
+    });
+////////////////////////////////end of dog routes///////////////////////////////////////////
+
+//////////////////////////////////PARKS routes//////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////
+router.route('/park')
+//adding a friend to a dogs friend list
+    .post(function(req, res){
+        var park = new Park(); // create the instance of the dog model
+
+        park.name = req.body.name;
+        park.suburb = req.body.suburb;
+        park.leash_status = req.body.leash_status;
+        park.leash_times = req.body.leash_times;
+        park.road_side = req.body.road_side;
+
+        //save the dog and check for errors
+        park.save( function(err) {
+            if(err){
+                res.send(err);//send error
+            }
+            //send successful response
+            res.json({message: 'Park created!'});
+        });
+    })
+
+    .get(function(req, res){
+        Park.find(function(err, parks){
+            if(err)res.send(err);
+
+            res.json(parks);
+        });
+    });
+
+
+//for park routes involving updating a single entry
+router.route('/parks/:park_id')
+    .get( function(req, res){
+
+        //lets convert into an object id
+        var ObjectId = mongoose.Types.ObjectId;
+        var id = new ObjectId(req.params.park_id.toString());
+
+
+        Park_Model.findById( id , function(err, park){
+            if(err){res.send(err);}
+
+            res.json(park);
+
+        });
+
+
+    })
+    //update the park with new info
+    .put(function(req, res){
+        // use our park model to find the park we want
+        //lets convert into an object id
+        var ObjectId = mongoose.Types.ObjectId;
+        var id = new ObjectId(req.params.park_id.toString());
+
+
+        Park_Model.findById( id , function(err, dog){
+            if(err){res.send(err);}
+
+            if(req.body.name!==undefined){
+                park.name = req.body.name;
+                console.log("updated name")
+            }
+
+            else if(req.body.suburb!==undefined){
+                park.suburb = req.body.suburb;
+                console.log("updated picture")
+            }
+
+            else if(req.body.leash_status!==undefined){
+                park.leash_status = req.body.leash_status;
+                console.log("updated description")
+            }
+
+
+            else if(req.body.leash_times!==undefined){
+                park.leash_times = req.body.leash_times;
+                console.log("updated breed")
+            }
+
+            else if(req.body.road_side!==undefined){
+                park.road_side = req.body.road_side;
+                console.log("updated breed")
+            }
+
+            // save the dog
+            dog.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json(park);
+            });
+
+
+        });
+
+    });
+
+
+/*name: String,
+        suburb: String,
+        leash_status: String,
+        dogs_there: [String],
+        leash_times: String,
+        road_side: Boolean*/
+
+router.route('/parks/:park_id/checkin/:dog_id')
+//adding a friend to a dogs friend list
+    .put(function(req, res){
+        // use our dog model to find the dog we want
+        //lets convert into an object id
+        var ObjectId = mongoose.Types.ObjectId;
+        var id = new ObjectId(req.params.park_id.toString());
+
+
+        Dog_Model.findById( id , function(err, park){
+
+            park.dogs_there.push(req.params.dog_id.toString());
+
+            // save the dog
+            park.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json(park);
+            });
+        })
 
     });
 
